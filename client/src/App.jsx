d639de53c55efaf5433a00db8d27e54238f5cb4f
Vishwas-0612 <srivastavavishwas01@gmail.com>
2025-07-13 19:15:44 +0530

    Setting Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { createRoutesFromElements } from "react-router";
+import { createBrowserRouter, Route, RouterProvider } from "react-router";
+import Layout from "./Layout";
+import HomePage from "./pages/HomePage";
+import PolaroidGenerator from "./pages/PolaroidGenerator";
+import MorseCode from "./pages/MorseCode";
+import MusicPlayer from "./pages/MusicPlayer";
 import "./App.css";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route path="" element={<HomePage />} />
+        <Route path="polaroid" element={<PolaroidGenerator />} />
+        <Route path="morse" element={<MorseCode />} />
+        <Route path="music" element={<MusicPlayer />} />
+      </Route>
+    )
+  );
   return <h1>Start</h1>;
 }
 
